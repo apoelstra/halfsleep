7e3c352920d8438e818ef07ad9320f903bfbd00d
Andrew Poelstra <apoelstra@wpsoftware.net>
2015-04-15 13:17:38 -0500

    Handle decorations on all types of items to prevent compiler warnings
    Still need to actually dupe trait and impl items...

--- src/lib.rs
+++ src/lib.rs
@@ -19,7 +19,7 @@ extern crate syntax;
 
 use std::mem;
 use rustc::plugin::Registry;
-use syntax::ext::base::Modifier;
+use syntax::ext::base::{Annotatable, MultiModifier};
 use syntax::parse::token;
 use syntax::ast;
 use syntax::codemap::Span;
@@ -37,30 +37,65 @@ mod test_duper;
 pub fn plugin_registrar(reg: &mut Registry) {
     reg.register_syntax_extension(
         token::intern("mutation_test"),
-        Modifier(Box::new(expand_mutation_test)));
+        MultiModifier(Box::new(expand_mutation_test)));
+}
+
+/// emit a warning for #[mutation_test] markers on things which don't use them
+fn complain_if_useless_annotation(cx: &mut ExtCtxt, item: &Annotatable) {
+    let (warn, sp) = match *item {
+        Annotatable::Item(ref i) => {
+            (match i.node {
+                 ast::Item_::ItemMod(_) | ast::Item_::ItemFn(_, _, _, _, _) => false,
+                 _ => true
+             }, i.span)
+        },
+        Annotatable::TraitItem(ref i) => {
+            (match i.node {
+                 ast::TraitItem_::MethodTraitItem(_, _) => false,
+                 _ => true
+             }, i.span)
+        },
+        Annotatable::ImplItem(ref i) => {
+            (match i.node {
+                 ast::ImplItem_::MethodImplItem(_, _) => false,
+                 _ => true
+             }, i.span)
+        }
+    };
+
+    if warn {
+        cx.span_warn(sp, "#[mutation_test] has no effect except on modules and functions");
+    }
 }
 
 /// Create several mutated copies of a given function
 pub fn expand_mutation_test(cx: &mut ExtCtxt, _span: Span,
-                            _meta: &ast::MetaItem, item: P<ast::Item>)
-                           -> P<ast::Item> {
+                            _meta: &ast::MetaItem, item: Annotatable)
+                           -> Annotatable {
+
+    complain_if_useless_annotation(cx, &item);
 
     // Check something sane has been decorated
-    let this_mod = if let ast::ItemFn(_, _, _, _, _) = item.node {
-        // We do nothing directly for decorated functions; when looking at
-        // a module we make a note of which ones are decorated, then consider
-        // them all at once. This lets us (a) keep track of how many should-fail
-        // test have been created and (b) locate all the unit tests in the
-        // first place.
-        return item;
-    } else if let ast::ItemMod(ref this_mod) = item.node {
-        this_mod
+    let this_mod = if let Annotatable::Item(ref ast_item) = item {
+        if let ast::ItemMod(ref this_mod) = ast_item.node {
+            this_mod
+        } else {
+            // We do nothing directly for decorated functions; when looking at
+            // a module we make a note of which ones are decorated, then consider
+            // them all at once. This lets us (a) keep track of how many should-fail
+            // test have been created and (b) locate all the unit tests in the
+            // first place.
+            // TODO: this clone is a lazy way around lack of non-lexically scoped borrows
+            return item.clone();
+        }
     } else {
-        cx.bug("#[mutation_test] can only be applied to functions and modules")
+        // TODO: this clone is a lazy way around lack of non-lexically scoped borrows
+        return item.clone();
     };
 
     // At this point we know we are looking at a module
     // Locate all the decorated functions
+    let item = if let Annotatable::Item(i) = item { i } else { unreachable!() };
     let mut loc = locator::Locator::new();
     loc.visit_item(&*item);
 
@@ -79,6 +114,6 @@ pub fn expand_mutation_test(cx: &mut ExtCtxt, _span: Span,
     // Add new unit tests...
     let mut test_duper = test_duper::TestDuper::new(&loc);
     // ...and replace the module in the AST
-    P(test_duper.fold_item_simple(item))
+    Annotatable::Item(P(test_duper.fold_item_simple(item)))
 }
 
